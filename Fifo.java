a1aff3e9595d3beaa81ef766f305b0071f8569c2
Pasquo <106750958+Pasquo05@users.noreply.github.com>
2023-01-28 10:06:38 +0100

    Add files via upload

---
+++ Fifo.java
@@ -0,0 +1,21 @@
+
+public class Fifo extends Pila {
+
+	public String pop() {
+		
+		
+		stack.remove()
+		
+		
+	}
+	
+	public void push(String input) {
+		
+		stack.add(input);
+		
+	}
+	
+	
+	
+	
+}
